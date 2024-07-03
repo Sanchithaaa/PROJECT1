a7f27969680388b896f02b9eda8a374fc662ce63
Sanchitha Gowda <142650502+Sanchithaaa@users.noreply.github.com>
2024-07-03 12:04:50 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,10 @@
+
+    function Calculator() {
+    return(<title>Calculator</title>)
+    "prime:'p'=[1,2,3,5,7,11]",
+    "fibonacci:'f'=[1,2,3,4,6,8]",
+    "even:'e'=[2,4,6,8,10]",
+    "random:'r' =[1,2,3,4,5,6,7,8,9,10]"
+    }
+export default Calculator;
+
